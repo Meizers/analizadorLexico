4ca97807e17e86c58c1de1849ab27da8b904b299
Tomas Resnik <101259872+Meizers@users.noreply.github.com>
2024-10-09 19:33:36 -0300

    Delete analizadorLexico-master directory

--- analizadorLexico-master/src/main/java/analizadorLexico/Main.java
+++
@@ -1,8 +0,0 @@
-package analizadorLexico;
-import vista.VentanaPrincipal;
-
-public class Main {
-    public static void main(String[] args) {
-        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-    }
-}
