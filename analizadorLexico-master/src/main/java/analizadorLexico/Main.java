fc1044185568fb1892b98f9f63ad3fb524ce4384
Tomas Resnik <101259872+Meizers@users.noreply.github.com>
2024-10-09 15:39:23 -0300

    VersionMeizers 1.0

---
+++ analizadorLexico-master/src/main/java/analizadorLexico/Main.java
@@ -0,0 +1,8 @@
+package analizadorLexico;
+import vista.VentanaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+    }
+}
